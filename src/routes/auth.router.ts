f3e679e14122af83d9f6ca370a361ca58638c927
jorgezerpa <jorgezerpacoder@gmail.com>
2022-10-10 17:13:27 -0400

    incorporate JWT

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,12 +1,18 @@
 import express, {Request, Response, NextFunction} from 'express';
 import passport from 'passport';
+import jwt from 'jsonwebtoken'
 const router = express.Router();
 
 router.post('/login',
   passport.authenticate('local', {session: false}),
   async (req:Request, res:Response, next:NextFunction) => {
     try {
-      res.json(req.user);
+        const payload = {
+            something: '10'
+        }
+        const secret = 'soy un secreto super seguro yeaahhh'
+        const token = jwt.sign(payload, secret)
+        res.json({token});
     } catch (error) {
       next(error);
     }
