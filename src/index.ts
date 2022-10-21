1d3cf58a4a57a70d1e8d7f581a12d483758b1982
jorgezerpa <jorgezerpacoder@gmail.com>
2022-10-21 07:16:14 -0400

    solve cors bad implementation

--- src/index.ts
+++ src/index.ts
@@ -11,13 +11,13 @@ dotenv.config()
 const app:Application = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors()) 
 app.use(express.json({limit: '50mb'})) 
 app.use(cookieParser())
 app.use(express.urlencoded({extended:false}))
 
 authInit()
 routerApi(app);
-cors()
 
 app.use(logErrors);
 app.use(boomErrorHandler);
