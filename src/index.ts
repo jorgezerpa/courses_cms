82a010fdc4692d920a26ccdbf0fc5a8be5984871
jorgezerpa <jorgezerpacoder@gmail.com>
2022-10-22 15:36:25 -0400

    allow credentials in CORS

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ dotenv.config()
 const app:Application = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors()) 
+app.use(cors({credentials:true})) 
 app.use(express.json({limit: '50mb'})) 
 app.use(cookieParser())
 app.use(express.urlencoded({extended:false}))
