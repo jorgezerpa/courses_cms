da368507a48b058d75f8cd294f94fa72be5522a1
jorgezerpa <jorgezerpacoder@gmail.com>
2022-11-23 07:17:06 -0400

    changes on config.ts file

--- src/config.ts
+++ src/config.ts
@@ -3,29 +3,11 @@ dontenv.config()
 
 const config = {
     PORT:parseInt(process.env.PORT as string),
-    
     DB_HOST: process.env.DB_HOST,
     DB_PORT: parseInt(process.env.DB_PORT as string),
     DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD,
     DB_NAME:process.env.DB_NAME,
-
-    JWT_SECRET:process.env.JWT_SECRET as string,
-    JWT_REFRESH_SECRET:process.env.JWT_REFRESH_SECRET as string,
-    JWT_RECOVERY_SECRET:process.env.JWT_RECOVERY_SECRET as string,
-
-    GMAIL_KEY: process.env.GMAIL_KEY,
-
-    DOMAIN_BASE_PATH: process.env.DOMAIN_BASE_PATH,
-
-    PAYPAL_USER_ID: process.env.PAYPAL_USER_ID,
-    PAYPAL_SECRET: process.env.PAYPAL_SECRET,
-    PAYPAL_API: process.env.PAYPAL_API,
-
-    CLOUDINARY_NAME:process.env.CLOUDINARY_NAME,
-    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-    CLOUDINARY_API_SECRET:process.env.CLOUDINARY_API_SECRET,
-
 }
 
 export default config
