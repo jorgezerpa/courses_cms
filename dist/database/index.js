bd9197e07d4547a9ba7193111ad90c565cf215eb
Jorge Zerpa <jorgezerpacoder@gmail.com>
2023-02-15 19:42:14 -0400

    delete dist folder

--- dist/database/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const data_source_1 = require("./data-source");
-//other connections. example -> import { AppDataSourceDev } from "./data-sourceDev"; 
-//export the one you will use
-exports.default = data_source_1.AppDataSource;
